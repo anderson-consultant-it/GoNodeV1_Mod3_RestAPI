81d1452b066adfa70cca267133e0cb5b7e2173b4
AndersonDaSilvaOSF <43174799+AndersonDaSilvaOSF@users.noreply.github.com>
2020-03-21 16:44:29 -0400

    Removed fixed PORT to it works on heroku

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use('/api', require('./app/routes'));
 
 app.use(sentry.Handlers.errorHandler());
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
 
 module.exports = app;
